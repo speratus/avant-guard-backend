459365245b87c5565d6eba557c6936ac3a8a7f1f
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-19 15:59:49 -0500

    adds associations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
     attr_encrypted :genius_token, key: ENV['DB_SECRET']
+
+    has_one :ranking
+
+    has_many :genre_scores, dependent: :destroy
+
+    has_many :games, dependent: :destroy
+
+    has_many :friendships, foreign_key: :friender_id, dependent: :destroy
+    has_many :friends, through: :friendships, source: :friended
 end
