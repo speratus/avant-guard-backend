abe5a0481c0bba77140a7b9f7a8cdac0faac8c3e
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-19 16:54:15 -0500

    adds user test for ranking

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'via associations' do 
+    it 'can access one ranking' do
+      user = User.new
+
+      expect(user).to respond_to(:ranking)
+      ranking = Ranking.new(total_score: 100, rank: 1)
+      user.ranking = ranking
+
+      expect(user.ranking).to eq(ranking)
+      expect(user.ranking.total_score).to eq 100
+      expect(user.ranking.rank).to eq 1
+    end
+  end
 end
