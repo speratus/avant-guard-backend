0d7d70e43b171f84c336f1163cbaaa88f59fdb35
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-19 17:07:17 -0500

    adds games spec for having questions

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'via associations' do
+    it 'has many questions' do
+      game = Game.new
+
+      3.times do
+        game.questions.build
+      end
+
+      expect(game.questions.length).to eq 3
+    end
+  end
 end
