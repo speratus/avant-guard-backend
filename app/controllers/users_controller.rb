f1e3987e43559bfc36ee49dbe967295062e2007e
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-20 13:24:58 -0500

    adds base user controller functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,58 @@
 class UsersController < ApplicationController
+    before_action :verify_authentication, except: [:create]
 
     def create
-        
+        user = User.new(user_params)
+
+        if user.save
+            render json: user
+        else
+            render json: {
+                message: 'Failed to create user',
+                errors: user.errors.full_messages
+            }
+        end
     end
 
     def show
-
+        render json: @user
     end
 
     def update
+        @user.update_attributes(user_params)
 
+        if @user.save
+            render json: @user
+        else
+            render json: {
+                message: "Update failed",
+                errors: @user.errors.full_messages
+            }
+        end
     end
 
     def destroy
-
+        if @user.destroy!
+            render json: {
+                message: 'successfully deleted user!',
+                user: @user
+            }
+        else
+            render json: {
+                message: 'failled to delete user',
+                user: @user
+            }
+        end
     end
 
     private
 
     def user_params
         params.require(:user).permit(:name, :username, :password, :password_confirmation, :genius_token)
+    end
+
+    def verify_authentication
+        user = User.find_by(id: params[:id])
+        @user = check_authorization(user, current_user)
     end
 end
