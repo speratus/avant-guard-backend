63d0d4d8c5f8235fd0e5c35b791663a2976f570a
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-20 11:25:10 -0500

    adds basic game authorization

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,5 +6,12 @@ class Game < ApplicationRecord
     has_many :questions, dependent: :destroy
 
     validates :multiplier, :user, :song, :genre, presence: true
-    
+
+    check_perm 'games#create' do |game, user|
+        !user.nil?
+    end
+
+    check_perm 'games#update', 'games#show', 'games#index' do |game, user|
+        game.user == user
+    end
 end
