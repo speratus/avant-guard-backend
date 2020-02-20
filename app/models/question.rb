fd9498d7c80d7f62db2ffd7ee9f31baa29beeaa1
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-20 11:11:22 -0500

    adds basic questions authorization

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,8 @@ class Question < ApplicationRecord
     belongs_to :game
 
     validates :answer, :game, presence: true
+
+    check_perm 'questions#check' do |question, user|
+        question.game.user == user
+    end
 end
