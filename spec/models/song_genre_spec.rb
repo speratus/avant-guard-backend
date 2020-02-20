8a04a482a351599de227cdbe1c4a9c8c7622bb3e
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-19 22:10:54 -0500

    adds song_genre spec for belonging to song

---
+++ spec/models/song_genre_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe SongGenre, type: :model do
+  context 'via associations' do
+    it 'belongs to a song' do
+      song_genre = SongGenre.new
+      song = Song.new
+
+      expect {song_genre.song = song}.to_not raise_error(NoMethodError)
+
+      expect(song_genre.song).to eq(song)
+    end
+  end
+end
