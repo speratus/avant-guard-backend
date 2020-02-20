c1c4cea41e8b6ce9c7b0ddc25c29bd2a6ad9308e
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-20 11:21:40 -0500

    adds basic genre_scores authorization

--- app/models/genre_score.rb
+++ app/models/genre_score.rb
@@ -3,4 +3,8 @@ class GenreScore < ApplicationRecord
   belongs_to :user
 
   validates :genre, :user, presence: true
+
+  check_perm 'genre_scores#index' do |scores, user|
+    !user.nil?
+  end
 end
