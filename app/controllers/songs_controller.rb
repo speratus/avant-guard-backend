82edd88f2497e463179d1f4875cb5c38307ff56c
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-20 13:45:23 -0500

    fills out base songs controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,18 @@
 class SongsController < ApplicationController
+    before_action :authorize_song
 
     def show
-        
+        render base_song_info(song)
+    end
+
+    private
+
+    def authorize_song
+        song = Song.find_by(params[:id])
+        @song = check_authorization(song, current_user)
+    end
+
+    def base_song_info(song)
+        song.to_json(except: :created_at, :updated_at)
     end
 end
