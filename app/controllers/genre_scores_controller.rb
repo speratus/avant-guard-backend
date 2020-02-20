913b30a8cd3299f400c475c9abfcf5084c2ba80d
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-20 15:18:14 -0500

    fills out basic genre scores functionality

--- app/controllers/genre_scores_controller.rb
+++ app/controllers/genre_scores_controller.rb
@@ -1,6 +1,13 @@
 class GenreScoresController < ApplicationController
 
     def index
-        
+        genres = check_authorization(Genre.where(user_id: params[:user_id]), current_user)
+        render json: basic_genre_data(genres)
+    end
+
+    private
+
+    def basic_genre_data(genre)
+        genre.as_json(only: [:score], include: {genre: {only: [:name]}})
     end
 end
