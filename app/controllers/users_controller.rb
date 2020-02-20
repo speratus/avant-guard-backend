b9e4bc7cfcecf999320608f01e35c8d41b920dd8
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-20 10:43:35 -0500

    adds user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
     def create
-
+        
     end
 
     def show
@@ -14,5 +14,11 @@ class UsersController < ApplicationController
 
     def destroy
 
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :username, :password, :password_confirmation, :genius_token)
     end
 end
