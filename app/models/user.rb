a70ea5888d259c399d041f13b97eba825420a491
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-20 11:06:33 -0500

    adds basic user authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,11 @@ class User < ApplicationRecord
     validates :name, :username, :password, :genius_token, presence: true
     validates :username: uniqueness: true
 
+    check_perm 'users#show' do |user, current_user|
+        !current_user.nil?
+    end
+
+    check_perm 'users#destroy', 'users#update' do |user, current_user|
+        user == current_user
+    end
 end
