b3544333385921a912a670d164512f64bd7eb4c4
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-20 10:53:19 -0500

    adds song validations

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,13 @@ class Song < ApplicationRecord
     has_many :genres, through: :song_genres
 
     has_many :games
+
+    validates :title, :artist, :release_date, presence: true
+    validate :has_at_least_one_genre
+    
+    def has_at_least_one_genre
+        if song.genres.length < 1
+            errors[:genres] << 'Songs must have at least one genre'
+        end
+    end
 end
