c4ba437e26c352bf956c0433bd6614c1323576cf
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-19 22:15:06 -0500

    adds song_genre spec for belonging to a genre

--- spec/models/song_genre_spec.rb
+++ spec/models/song_genre_spec.rb
@@ -10,5 +10,14 @@ RSpec.describe SongGenre, type: :model do
 
       expect(song_genre.song).to eq(song)
     end
+
+    it 'belongs to a genre' do
+      song_genre = SongGenre.new
+      genre = Genre.new
+
+      expect {song_genre.genre = genre}.to_not raise_error(NoMethodError)
+
+      expect(song_genre.genre).to eq(genre)
+    end
   end
 end
