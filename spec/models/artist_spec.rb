9bae0cd0013bdb719e1d6119df54f1ea223576ae
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-21 11:33:29 -0500

    adds artist spec for having many genres

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'via associations' do
+    it 'has many songs' do
+      artist = Artist.new
+
+      4.times do
+        artist.songs.build
+      end
+
+      expect(artist.songs.length).to eq 4
+    end
+
+    it 'has many genres' do
+      artist = Artist.new
+
+      5.times do
+        song = artist.songs.build(title: 'test')
+
+        rand(1..6).times do
+          g = song.genres.build(name: 'demo')
+          g.save
+        end
+
+        song.save
+        # puts "It has genres: #{song.genres.map {|g| g.name}}"
+      end
+
+      artist.save
+
+      # puts "There are #{Genre.all.length} genres. They are #{Genre.all.map {|g| g.name}}"
+
+      # puts "artist: #{artist}. artist songs: #{artist.songs.map {|s| s.title}}. artist genres: #{artist.genres.map {|g| g.name}}"
+
+      # artist.songs.each do |s|
+      #   puts "song #{s.title} has #{s.genres.length} genres. they are #{s.genres.map {|g| g.name}}."
+      # end
+
+      expect(artist.genres.length).to be > 5
+    end
+  end
 end
