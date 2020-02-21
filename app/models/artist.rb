4790d84eb9c2fe20f655e31211b28332298499ac
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-21 11:34:02 -0500

    adds method to get many genres

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,13 @@
 class Artist < ApplicationRecord
     has_many :songs
 
-    has_many :genres, through: :songs
+    def genres
+        genres = []
+        self.songs.each do |s|
+            s.genres.each do |g|
+                genres << g unless genres.include?(g)
+            end
+        end
+        genres
+    end
 end
