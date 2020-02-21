e9e52fb3f18bf0a252a419466acf6651d61213f2
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-21 14:43:42 -0500

    adds missing column to question

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_21_192016) do
+ActiveRecord::Schema.define(version: 2020_02_21_194251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2020_02_21_192016) do
     t.bigint "game_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "question_type"
     t.index ["game_id"], name: "index_questions_on_game_id"
   end
 
