dbb638a6d55832b822a2f831bda9b22a3f3ca9bf
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-27 14:42:12 -0500

    adds route for genre scores

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, except: [:index]
+  resources :users, except: [:index] do
+    resources :genre_scores, only: :index
+  end
   resources :sessions, only: [:create]
   resources :genres, only: [:index]
   resources :artists, only: [:index]
