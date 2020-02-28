ea20c20448b3baf92c37cc344f9171a8829b050a
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-28 11:31:38 -0500

    adds route for friends action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users do
     resources :genre_scores, only: :index
+
+    get 'friends', on: :member
   end
   resources :sessions, only: [:create]
   resources :genres, only: [:index]
