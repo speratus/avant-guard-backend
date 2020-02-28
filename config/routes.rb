de797c49e0472d76d363efc690cd321d4ff24612
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-02-28 11:37:47 -0500

    adds resources for friendships

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   resources :artists, only: [:index]
   resources :games, except: [:delete]
   resources :rankings, only: [:index]
+  reources :friendships
   patch '/questions/:id', to: 'questions#check'
 end
